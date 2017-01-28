ea798056d4158c079d93e44794aa5294ac13da81
Penghuan Ni <nipenghuan@gmail.com>
2017-01-28 15:18:56 -0600

    finish lab1

--- labs/percolation/Percolation.java
+++ labs/percolation/Percolation.java
@@ -1,36 +1,80 @@
-
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.*;
-
+import java.lang.*;
 
 public class Percolation {
-   // public Percolation(int n) {               // create n-by-n grid, with all sites blocked
-   //
-   // }
-   //
-   // public void open(int row, int col) {   // open site (row, col) if it is not open already
-   //
-   // }
-   //
-   // public boolean isOpen(int row, int col) {  // is site (row, col) open?
-   //
-   // }
-   //
-   // public boolean isFull(int row, int col) {  // is site (row, col) full?
-   //
-   // }
-   //
-   // public int numberOfOpenSites() {      // number of open sites
-   //
-   // }
-   //
-   // public boolean percolates() {              // does the system percolate?
-   //
-   // }
+   // private data fileds
+   private WeightedQuickUnionUF wquf, wquf2;
+   private boolean[] parent;
+   private int size;
+   private int open_count;
+
+   public Percolation(int n) {               // create n-by-n grid, with all sites blocked
+       // if n is equals or small to 0, throw an illegal argument exception
+       if (n<=0)
+          throw new IllegalArgumentException("illegal argument, size shoule be greater than 0");
+       size = n;
+       wquf = new WeightedQuickUnionUF(n*n+2); // 0..n*n+1 with virtual top and virtual bottom
+       wquf2 = new WeightedQuickUnionUF(n*n+1); // 2nd uf to avoid backwash
+       parent = new boolean[n*n];
+   }
+
+   public void open(int row, int col) {   // open site (row, col) if it is not open already
+       if (row<=0 || row>size || col<=0 || col>size)
+          throw new IndexOutOfBoundsException("row and col should between 1 and " + size);
+       int index = xyTo1D(row, col);
+
+       if (!parent[index]){
+           parent[index] = true;
+           open_count += 1;
+       }
+
+       if (row==1) wquf.union(0, index+1);
+       if (row==1) wquf2.union(0, index+1);
+       if (row==size) wquf.union(index+1, size*size+1);
+
+       int connect1 = row-1>0 ? xyTo1D(row-1, col) : index;
+       int connect2 = row+1<=size ? xyTo1D(row+1, col) : index;
+       int connect3 = col-1>0 ? xyTo1D(row, col-1) : index;
+       int connect4 = col+1<=size ? xyTo1D(row, col+1) : index;
+
+       if (connect1 >= 0 && connect1 < size*size && parent[connect1]) {wquf.union(index+1, connect1+1);wquf2.union(index+1, connect1+1);}
+       if (connect2 >= 0 && connect2 < size*size && parent[connect2]) {wquf.union(index+1, connect2+1);wquf2.union(index+1, connect2+1);}
+       if (connect3 >= 0 && connect3 < size*size && parent[connect3]) {wquf.union(index+1, connect3+1);wquf2.union(index+1, connect3+1);}
+       if (connect4 >= 0 && connect4 < size*size && parent[connect4]) {wquf.union(index+1, connect4+1);wquf2.union(index+1, connect4+1);}
+   }
+
+   public boolean isOpen(int row, int col) {  // is site (row, col) open?
+       if (row<=0 || row>size || col<=0 || col>size)
+          throw new IndexOutOfBoundsException("row and col should between 1 and " + size);
+       return parent[xyTo1D(row, col)];
+   }
+
+   public boolean isFull(int row, int col) {  // is site (row, col) full?
+       if (row<=0 || row>size || col<=0 || col>size)
+          throw new IndexOutOfBoundsException("row and col should between 1 and " + size);
+       // if it is connect with virtual top open site
+       return wquf2.connected(0, xyTo1D(row, col)+1);
+   }
+
+   public int numberOfOpenSites() {      // number of open sites
+       return open_count;
+   }
+
+   public boolean percolates() {              // does the system percolate?
+       return wquf.connected(0, size*size+1);
+   }
+
+   // helper function
+   private int xyTo1D(int x, int y) {
+       return (x-1) * size + y - 1;
+   }
 
    public static void main(String[] args) {   // test client (optional)
        StdOut.println("hello princeton");
+       Percolation p = new Percolation(4);
+       p.open(-2, 1);
    }
 }
--- labs/percolation/PercolationStats.java
+++ labs/percolation/PercolationStats.java
@@ -1,23 +1,74 @@
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+import edu.princeton.cs.algs4.*;
+import java.lang.*;
+import java.util.*;
 
 public class PercolationStats {
 
+   private double[] thresholds;
+   private double mean, stddev, confidenceLo, confidenceHi;
+   private int T;
+
    public PercolationStats(int n, int trials) {   // perform trials independent experiments on an n-by-n grid
+       if (n<=0||trials<=0)
+          throw new IllegalArgumentException("illegal argument, size shoule be greater than 0");
+       T = trials;
+       thresholds = new double[trials];
+       for (int i=0; i<trials; i++) {
+           Percolation perc = new Percolation(n);
+           int count = 0, row, col, threshold;
+           while (!perc.percolates()) {
+               row = StdRandom.uniform(1, n+1);
+               col = StdRandom.uniform(1, n+1);
+               if (!perc.isOpen(row, col)) {
+                   perc.open(row, col);
+                   count += 1;
+               }
+           }
+           thresholds[i] = (double) count / (n*n);
+       }
+       // calculate mean
+       double sum = 0;
+       for (double threshold : thresholds) {
+           sum += threshold;
+       }
+       mean = sum / T;
+       // calculate stddev
+       double stdsum = 0;
+       for (double threshold : thresholds) {
+           stdsum += (threshold - mean)*(threshold - mean);
+       }
+       stddev = Math.sqrt(stdsum / (T-1));
+       // calculate confidence interval
+       confidenceLo = mean - 1.96 * stddev / Math.sqrt(T);
+       confidenceHi= mean + 1.96 * stddev / Math.sqrt(T);
    }
 
    public double mean() {                         // sample mean of percolation threshold
+       return mean;
    }
 
    public double stddev() {                       // sample standard deviation of percolation threshold
+       return stddev;
    }
 
    public double confidenceLo() {                 // low  endpoint of 95% confidence interval
+       return confidenceLo;
    }
 
    public double confidenceHi() {                 // high endpoint of 95% confidence interval
+       return confidenceHi;
    }
 
    public static void main(String[] args) {       // test client (described below)
+       PercolationStats s = new PercolationStats(200, 10);
+       StdOut.println("mean is: " + s.mean);
+       StdOut.println("stddev is: " + s.stddev);
+       StdOut.println("CI is: [" + s.confidenceLo + ", " + s.confidenceHi + "]");
+    //    StdOut.println();
    }
 
 }
